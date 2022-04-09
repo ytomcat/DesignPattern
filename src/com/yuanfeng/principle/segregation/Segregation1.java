e1a41ea13f32f511008232a9b4712da81b3bcf77
yuan <yuanfeng@gongsibao.com>
2022-04-09 12:48:30 +0800

    first commit

---
+++ src/com/yuanfeng/principle/segregation/Segregation1.java
@@ -0,0 +1,16 @@
+package com.yuanfeng.principle.segregation;
+
+/**
+ * @author yuanfeng
+ * @version 1.0.0
+ * @ClassName Segregation1.java
+ * @Description TODO
+ * DesignPattern
+ * @createTime 2022年04月09日 12:46:00
+ */
+public class Segregation1 {
+
+}
+interface Interface1{
+
+}
